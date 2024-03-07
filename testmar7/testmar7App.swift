64374b95a01646f1954249749702b64d62fc6c68
RileyK19 <rileykoo19@gmail.com>
2024-03-07 09:22:05 -0600

    Initial Commit

---
+++ testmar7/testmar7App.swift
@@ -0,0 +1,17 @@
+//
+//  testmar7App.swift
+//  testmar7
+//
+//  Created by Riley Koo on 3/7/24.
+//
+
+import SwiftUI
+
+@main
+struct testmar7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
